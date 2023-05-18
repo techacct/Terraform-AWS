fe63b8249468c6462f731a5fb4c80d2bc92a246e
techacct <ademolaoladapo@gmail.com>
2023-05-18 14:14:29 +0100

    private instances added

--- instance.tf
+++ instance.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "public" {
-  ami                                = "ami-02396cdd13e9a1257"
-  associate_public_ip_address        = true
-  instance_type                      = "t3.micro"
-  key_name                           = "mykey"
-  vpc_securityvpc_security_group_ids = [aws_security_group.public]
-  subnet_id                          = aws_subnet.public[0].id
+  ami                         = "ami-0a79730daaf45078a"
+  associate_public_ip_address = true
+  instance_type               = "t3.micro"
+  key_name                    = "mykey"
+  vpc_security_group_ids      = [aws_security_group.public.id]
+  subnet_id                   = aws_subnet.public[0].id
 
   tags = {
     Name = "${var.env_code}-public"
@@ -14,24 +14,61 @@ resource "aws_instance" "public" {
 resource "aws_security_group" "public" {
   name        = "${var.env_code}-public"
   description = "Allow inbound traffic"
-  vpc_id        = aws_vpc.main.id
+  vpc_id      = aws_vpc.main.id
 
-  ingress = {
+  ingress {
     description = "SSH from public"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks  = ["41.79.5.2/32"]
+    cidr_blocks = ["41.79.5.2/32"]
   }
 
-  egress = {
-    from_port  = 0
-    to_port    = 0
-    protocol   = "-1"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
     Name = "${var.env_code}-public"
+  }
+}
+
+resource "aws_instance" "private" {
+  ami                    = "ami-0a79730daaf45078a"
+  instance_type          = "t3.micro"
+  key_name               = "mykey"
+  vpc_security_group_ids = [aws_security_group.private.id]
+  subnet_id              = aws_subnet.private[0].id
+
+  tags = {
+    Name = "${var.env_code}-public"
+  }
+}
+
+resource "aws_security_group" "private" {
+  name        = "${var.env_code}-private"
+  description = "Allow VPC traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env_code}-private"
   }
 }
