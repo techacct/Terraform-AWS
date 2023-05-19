4ebdb483fe24bd3bc2c6395e1a5d0f7fbc1c2216
Ademola Oladapo <ademolaoladapo@gmail.com>
2023-05-19 09:01:51 +0100

    Ec2 (#8)
    * creating an instance
    
    * private instances added
    
    * extra lines added

--- vpc.tf
+++ vpc.tf
@@ -4,37 +4,37 @@ locals {
 }
 
 resource "aws_vpc" "main" {
-    cidr_block = var.vpc_cidr 
+  cidr_block = var.vpc_cidr
 
-    tags = {
+  tags = {
     Name = var.env_code
-    }
+  }
 }
 
 
 resource "aws_subnet" "public" {
   count = length(var.public_cidr)
 
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.public_cidr[count.index]
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_cidr[count.index]
   availability_zone = local.availability_zones[count.index]
 
   tags = {
-    Name = "${var.env_code}-public${count.index+1}"
+    Name = "${var.env_code}-public${count.index + 1}"
   }
-} 
+}
 
 resource "aws_subnet" "private" {
   count = length(var.private_cidr)
 
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.private_cidr[count.index]
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_cidr[count.index]
   availability_zone = local.availability_zones[count.index]
 
   tags = {
-    Name = "${var.env_code}-private${count.index+1}"
+    Name = "${var.env_code}-private${count.index + 1}"
   }
-} 
+}
 
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
@@ -67,19 +67,19 @@ resource "aws_route_table_association" "public" {
 resource "aws_eip" "nat" {
   count = length(var.public_cidr)
 
-  vpc      = true
-    tags = {
-    Name = "${var.env_code}-nat${count.index+1}"
+  vpc = true
+  tags = {
+    Name = "${var.env_code}-nat${count.index + 1}"
   }
 }
 resource "aws_nat_gateway" "main" {
-count = length(var.public_cidr)
+  count = length(var.public_cidr)
 
   allocation_id = aws_eip.nat[count.index].id
   subnet_id     = aws_subnet.public[count.index].id
 
   tags = {
-    Name = "${var.env_code}-main${count.index+1}"
+    Name = "${var.env_code}-main${count.index + 1}"
   }
 
   # To ensure proper ordering, it is recommended to add an explicit dependency
@@ -93,12 +93,12 @@ resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.main[count.index].id
   }
 
   tags = {
-    Name = "${var.env_code}-private${count.index+1}"
+    Name = "${var.env_code}-private${count.index + 1}"
   }
 }
 
